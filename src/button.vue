4675b068f900d3a8ae2973c59a541f6ae41cf078
HongKongsun <787874123@qq.com>
2020-05-15 09:25:39 +0800

    实现单文件组件

---
+++ src/button.vue
@@ -0,0 +1,34 @@
+<template>
+<!-- 放html -->
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {
+    // 放 js 代码
+    // 不需要Vue.conponent
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        .g-button:hover {
+        border-color: var(--border-color-hover);
+    }
+    // 套用 而且不需要重复 写当前选择器，而是用 & 代替
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+    &:focus {
+        outline: none;
+    }
+    }
+    
+</style>
